6ab130853760d20f570c63d07f7f1c874bff6f27
Cristián Donoso <cristiandonosoc@gmail.com>
2016-05-23 22:25:41 -0400

    Initial commit
    Basic Hexagonal code running

---
+++ Assets/Scripts/GridGeneration.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Assets.Scripts.Utils;
+
+public class GridGeneration : MonoBehaviour
+{
+
+    public Transform HexagonPrefab;
+
+    public int GridWidth;
+    public int GridHeight;
+
+    float _hexSide;
+    float _hexHalfHeight;
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer spriteRenderer = HexagonPrefab.GetComponent<SpriteRenderer>();
+
+        Bounds spriteBounds = spriteRenderer.sprite.bounds;
+        Vector3 hexagonExtents = spriteBounds.extents;
+
+        _hexSide = hexagonExtents.x;
+        _hexHalfHeight = (Mathf.Sqrt(3) * _hexSide) / 2;
+
+
+        Vector3 hexCoords = Vector3.zero;
+        Vector3 hexPosition = Vector3.zero;
+        // We instantiate the prefabs
+        for (int hY = 0; hY < GridHeight; ++hY)
+        {
+            for (int hX = 0; hX < GridWidth; ++hX)
+            {
+                hexCoords.x = hX;
+                hexCoords.y = hY;
+                HexCoords.HexToWorld(_hexSide, _hexHalfHeight,
+                                   hexCoords, ref hexPosition);
+                Transform t = (Transform)Instantiate(HexagonPrefab,
+                                                     hexPosition,
+                                                     Quaternion.identity);
+            }
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // We get mouse position
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rounded = Vector3.zero;
+        HexCoords.RoundHex(_hexSide, _hexHalfHeight, pos, ref rounded);
+
+        Debug.Log(string.Format("POS: {0}, ROUNDED: {1}", pos, rounded));
+	}
+}
---
+++ Assets/Scripts/Utils/HexMath.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+    internal static class HexCoords
+    {
+        internal static void HexToWorld(float hexSide, float hexHalfHeight,
+                        Vector3 hexCoords, ref Vector3 worldCoords)
+        {
+            worldCoords.x = (3 * hexSide * hexCoords.x) / 2;
+            worldCoords.y = (hexHalfHeight * hexCoords.x ) + (2 * hexHalfHeight * hexCoords.y);
+        }
+
+        internal static void WorldToHex(float hexSide, float hexHalfHeight,
+                                        Vector3 worldCoords, ref Vector3 hexCoords)
+        {
+            hexCoords.x = 2 / (3 * hexSide);
+            hexCoords.y = (-1 / (3 * hexSide)) + (1 / (2 * hexHalfHeight));
+        }
+
+        internal static void RoundHex(float hexSide, float hexHalfHeight, 
+                                      Vector3 hexCoords, ref Vector3 roundedCoords)
+        {
+            if ((hexCoords.x < 0) || (hexCoords.y < 0))
+            {
+                // TODO(Cristian): Support negative hex coords
+                throw new System.Exception("NO NEGATIVE HEX COORDS SUPPORTED");
+            }
+
+            float scaledX = hexCoords.x / ((3 * hexSide) / 2);
+            float scaledY = hexCoords.y / (2 * hexHalfHeight);
+
+            float fX = Mathf.Floor(scaledX);
+            float fY = Mathf.Floor(scaledY);
+            float restX = scaledX - fX;
+            float restY = scaledY - fY;
+
+            if (restX > 0.5)
+            {
+                roundedCoords.x = fX + 1;
+                roundedCoords.y = fY;
+            }
+            else
+            {
+                if (restY > 0.5)
+                {
+                    if (restX > (restY / 2))
+                    {
+                        roundedCoords.x = fX + 1;
+                        roundedCoords.y = fY;
+                    }
+                    else
+                    {
+                        roundedCoords.x = fX;
+                        roundedCoords.y = fY + 1;
+                    }
+                }
+                else
+                {
+                    if (restX > (0.5 - (restY / 2)))
+                    {
+                        roundedCoords.x = fX + 1;
+                        roundedCoords.y = fY;
+                    }
+                    else
+                    {
+                        roundedCoords.x = fX;
+                        roundedCoords.y = fY;
+                    }
+                }
+            }
+        }
+    }
+}
