2170a7c38efa3e36ab367e198ea342fcb91154e7
Benjamin Fantini <bdfantini@gmail.com>
2016-05-26 21:32:35 -0400

    Added Party and Size objects

--- GameLogic/Map.cs
+++ GameLogic/Map.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameLogic.Utils;
 
 namespace GameLogic
 {
@@ -11,14 +12,28 @@ namespace GameLogic
         #region INTERFACE
 
         public Hexagon[] Grid { get { return _grid; } }
+        public ushort Turn { get { return _turn; } }
 
         #endregion
 
         // For now, keep it simple
         Hexagon[] _grid;
 
+        // Players parties
+        Party[] _party;
 
+        // Map size
+        Size _mapSize;
 
+        // This should be an object, with different states
+        ushort _turn;
 
+        public Map(Size mapSize, ushort partyNumber)
+        {
+            _grid = new Hexagon[mapSize.Width + mapSize.Height];
+            _party = new Party[partyNumber];
+            _mapSize = mapSize;
+            _turn = 0;
+        }
     }
 }
---
+++ GameLogic/Utils/Size.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic.Utils
+{
+    public class Size
+    {
+        #region INTERFACE
+
+        public ushort Width { get { return _width; } }
+        public ushort Height { get { return _height; } }
+
+        #endregion
+
+        ushort _width;
+        ushort _height;
+
+        public Size(ushort width, ushort height)
+        {
+            _width = width;
+            _height = height;
+        }
+    }
+}
