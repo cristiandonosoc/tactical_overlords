4448e2dec6157143c10e7eb66c99784d146bb6ca
Cristián Donoso <cristiandonosoc@gmail.com>
2016-05-26 20:40:22 -0400

    Added C# sln

---
+++ GameLogic/Map.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class Map
+    {
+        #region INTERFACE
+
+        public Hexagon[] Grid { get { return _grid; } }
+
+        #endregion
+
+        // For now, keep it simple
+        Hexagon[] _grid;
+
+
+
+
+    }
+}
