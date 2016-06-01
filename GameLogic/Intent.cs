f78302bb1f536d4db2ef4ddc4536311c7b78f6b9
Benjamin Fantini <bdfantini@gmail.com>
2016-05-31 21:37:05 -0400

    Renamed Action to Intent

--- GameLogic/Intent.cs
+++ GameLogic/Intent.cs
@@ -5,14 +5,15 @@ using System.Text;
 
 namespace GameLogic
 {
-    public enum Type
+    internal enum Type
     {
         Move,
         Skill, // basic attack should be treated as a skill
-        Interact // trying to open a chest or do something in the map
+        Interact, // trying to open a chest or do something in the map
+        EndTurn // triggered at the end of a player' s turn
     }
 
-    class Action
+    class Intent
     {
         #region MEMBERS
 
@@ -28,19 +29,22 @@ namespace GameLogic
         // Action's date
         DateTime _triggeredAt;
 
+        // Action's turn
+        ushort _turn;
+
         #endregion
 
         #region CONSTRUCTORS
 
-        public Action(Entity sourceEntity, Type type, Hexagon destination)
+        public Intent(Entity sourceEntity, Type type, Hexagon destination, ushort turn)
         {
             _sourceEntity = sourceEntity;
             _type = type;
             _destination = destination;
             _triggeredAt = DateTime.Now;
+            _turn = turn;
         }
 
         #endregion
-
     }
 }
